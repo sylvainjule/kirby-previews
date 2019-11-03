869380723e018678fa0d6f5801c0760cfd34eaaa
Sylvain Julé <sylvain.jule@free.fr>
2019-11-03 16:10:17 +0100

    Add optionnal alt Files preview. Closes #3

---
+++ src/components/FilesPreview.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul v-if="value" class="k-files-field-preview">
+    <li v-for="file in value" :key="file.url">
+      <k-link :title="file.filename" :to="file.link" @click.native.stop>
+        <figure class="files-preview">
+            <k-image v-if="file.type === 'image'" v-bind="imageOptions(file)" />
+            <k-icon v-else v-bind="file.icon" />
+            <figcaption v-html="file.filename"></figcaption>
+        </figure>
+      </k-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  extends: 'k-files-field-preview'
+};
+</script>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-//import TogglePreview       from './components/TogglePreview.vue'
+// import TogglePreview       from './components/TogglePreview.vue'
+// import FilesPreview        from './components/FilesPreview.vue'
 import LocatorPreview      from './components/LocatorPreview.vue'
 import ColorPreview        from './components/ColorPreview.vue'
 import ColorPalettePreview from './components/ColorPalettePreview.vue'
@@ -10,7 +11,8 @@ import './assets/svg/icons.js'
 
 panel.plugin('sylvainjule/previews', {
   components: {
-    //'k-toggle-field-preview': TogglePreview,
+    // 'k-toggle-field-preview': TogglePreview,
+    // 'k-files-field-preview': FilesPreview,
     'k-locator-field-preview': LocatorPreview,
     'k-color-field-preview': ColorPreview,
     'k-color-palette-field-preview': ColorPalettePreview,
