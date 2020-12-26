00cf64f5b1640b9119a3f062fbacf519a32e7f25
Sylvain Julé <sylvain.jule@free.fr>
2020-12-26 23:13:36 +0100

    Remove Locator preview (included in the Locator plugin directly)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // import TogglePreview       from './components/TogglePreview.vue'
 // import FilesPreview        from './components/FilesPreview.vue'
-import LocatorPreview      from './components/LocatorPreview.vue'
 import ColorPreview        from './components/ColorPreview.vue'
 import ColorsPreview       from './components/ColorsPreview.vue'
 import ColorPalettePreview from './components/ColorPalettePreview.vue'
@@ -8,13 +7,10 @@ import StructurePreview    from './components/StructurePreview.vue'
 import TagsPreview         from './components/TagsPreview.vue'
 import MultiselectPreview  from './components/MultiselectPreview.vue'
 
-import './assets/svg/icons.js'
-
 panel.plugin('sylvainjule/previews', {
   components: {
     // 'k-toggle-field-preview': TogglePreview,
     // 'k-files-field-preview': FilesPreview,
-    'k-locator-field-preview': LocatorPreview,
     'k-color-field-preview': ColorPreview,
     'k-colors-field-preview': ColorsPreview,
     'k-color-palette-field-preview': ColorPalettePreview,
